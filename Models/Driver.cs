47be90b6134fe203f3cd295de70d8b55a9bcb2d5
RafaelLibrero <librerorafael@outlook.com>
2025-05-04 06:06:20 +0200

    Removed models dependency and added them into the project

---
+++ Models/Driver.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BoxBoxModels
+{
+    [Table("Drivers")]
+    public class Driver
+    {
+        [Key]
+        [Column("DriverID")]
+        public int DriverID { get; set; }
+        [Column("DriverName")]
+        public string DriverName { get; set; }
+        [Column("CarNumber")]
+        public int CarNumber { get; set; }
+        [Column("TeamID")]
+        public int TeamID { get; set; }
+        [Column("Flag")]
+        public string Flag { get; set; }
+        [Column("Imagen")]
+        public string Imagen { get; set; }
+
+    }
+}
---
+++ Models/Team.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BoxBoxModels
+{
+    [Table("Teams")]
+    public class Team
+    {
+        [Key]
+        [Column("TeamID")]
+        public int TeamId { get; set; }
+        [Column("TeamName")]
+        public string TeamName { get; set; }
+        [Column("Logo")]
+        public string Logo { get; set; }
+    }
+}
